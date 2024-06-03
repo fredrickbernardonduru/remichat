9b037662002f906f956ee7247569dbc03e6d2027
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-06-03 12:53:57 +0300

    clerk update

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-const PrismaClientSingleton = () => {
+const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-type PrismaClientSingleton = ReturnType<typeof PrismaClientSingleton>;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? PrismaClientSingleton();
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
