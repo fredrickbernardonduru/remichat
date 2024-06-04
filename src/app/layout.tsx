641012f2243e2c6c7f9cfab69fc87c22555879c6
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-06-04 14:44:31 +0300

    Still working on authentification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-  
+    <ClerkProvider>
       <body className={inter.className}>{children}</body>
-   
+      </ClerkProvider>
   );
 }
