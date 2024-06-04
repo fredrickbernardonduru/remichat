db2220d1fe458cda6e8a9d8cc5701b64dafa3e68
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2024-06-04 15:01:40 +0300

    Re done the code to authentification

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignUp appearance={{ variables: {colorPrimary: "#0F172A"}}} />
+      <SignUp appearance={{ variables: { colorPrimary: "#0F172A" } }} />
     </div>
   );
 }
